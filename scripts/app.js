24cf31e4429acf15c1f7b044904e6355793074f2
csgill <christinegill32@gmail.com>
2020-05-12 16:43:11 -0700

    working click opacity and soup bowl

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,16 @@
+const $section = $('<section class="container"></section>');
+const $body = $('body');
+
+const makeGameBoard = () => {
+    $body.append($section);
+
+}
+makeGameBoard();
+
 const createBarriers = (num) => {
-    let i = 0;
-    for (i; i < num; i++) {
-        const $barrier = $(`<div class = "barrier" id='${i}'></div>`);
-        $('.container').append($barrier);
+    for (let i = 0; i < num; i++) {
+        const $barrier = $(`<article class = "barrier" id='${i}'></article>`);
+        $section.append($barrier);
     }
     let winner = Math.floor(Math.random() * num);
     const $randomElement = $(`#${winner}`);
@@ -11,12 +19,46 @@ const createBarriers = (num) => {
 };
 createBarriers(4);
 
-// const handleClick = event => {
-//     if (event.target.classList.contains("clicked") === false) {
-//         $(event.target).addClass("clicked");
+const $winner = $('article div#winner');
+console.log($winner);
+$winner.on("click", function (event) {
+    console.log("winner click");
+})
+
+
+const handleClick = event => {
+    // if (event.target.contains("div #winner")) {
+    //     $(event.target).addClass("appear")
+    // }
+    if ($(event.target).children().length) {
+        $(event.target).addClass("appear")
+        round++
+    } else if (event.target.classList.contains("clicked") === false) {
+        $(event.target).addClass("clicked");
+    }
+}
+$('.barrier').on('click', handleClick);
+
+// let round = 1;
+
+// const updateRound = () => {
+//     $('#round').text(`Round: ${round}`);
+// }
+
+// const setUpRound = () => {
+//     updateRound();
+//     $('.barriers').empty();
+//     if (round === 1) {
+//         createBarriers(5);
+//     } else if (round === 2) {
+//         createSquares(8);
+//     } else if (round === 3) {
+//         createSquares(10);
+//     } else {
+//         $('h1').text(`SOUP'S UP`);
 //     }
 // }
-// $('.barrier').on('click', handleClick);
+// setUpRound();
 
 
 /* This gives all but one the class "winner"*/
