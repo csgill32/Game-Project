6446f83dc0f1574fb8ab6012f5000d59b98f733b
csgill <christinegill32@gmail.com>
2020-05-13 11:40:02 -0700

    lives counting down but only resetting at round 1

--- scripts/app.js
+++ scripts/app.js
@@ -30,13 +30,17 @@ const updateLives = () => {
 
 const setUpRound = () => {
     updateRound();
+    // updateLives();
+    // lives = 3;
     $section.empty();
     if (round === 1) {
         createBarriers(5);
     } else if (round === 2) {
         createBarriers(8);
+        // lives = 3;
     } else if (round === 3) {
         createBarriers(10);
+        // lives = 3;
     } else {
         $('h1').text(`SOUP'S UP`);
     }
@@ -49,17 +53,31 @@ const handleClick = event => {
         $(event.target).addClass("appear");
         // window.alert('WIN!');
         round++;
+        // lives = 3;
         setUpRound();
+        updateLives();
         // stop round
         //move to next round
     } else $(event.target).addClass("clicked");
+    if ($(event.target.classList.contains("clicked"))) {
+        console.log("Bad click");
+        lives--;
+        updateLives();
 
+    }
     if ($('.clicked').length > 2) {
         round = 1;
+        lives = 3;
         setUpRound();
+        updateLives();
         // restart
     }
 }
 
 $section.on('click', 'article', handleClick);
 
+// for (i = 0; i <= $section.length; i++) {
+    //     if ($section.classList.contains("clicked")) {
+    //         lives--;
+    //     }
+    // }
